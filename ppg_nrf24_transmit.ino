c30168b870c983293852a0e2e5e41e3473070ac1
alexarah <sarahalexandraa6@gmail.com>
2026-08-02 18:21:24 +0700

    Update ppg_nrf24_transmit.ino

--- ppg_nrf24_transmit.ino
+++ ppg_nrf24_transmit.ino
@@ -40,7 +40,7 @@ void setup() {
 
   radio.setPALevel(RF24_PA_HIGH);
   radio.setDataRate(RF24_250KBPS);
-  radio.setChannel(76);
+  radio.setChannel(0);
   radio.setPayloadSize(sizeof(PPGPacket));
   radio.openWritingPipe(ADDRESS);
   radio.stopListening();
